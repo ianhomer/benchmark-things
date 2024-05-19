9e4d9346e3bbddc050dce313e1d5cf3561462617
Ian Homer <code@ianhomer.com>
2024-05-19 10:35:28 +0100

    ♻️ improve log output during run

--- benchmark-k8s-cluster-stacks.sh
+++ benchmark-k8s-cluster-stacks.sh
@@ -2,21 +2,23 @@
 
 set -e
 
+. ./time.sh
+
 function await() {
+  echo "awaiting $1"
   timeout 30 bash -c \
     "until $1 ; do sleep 0.1 ; done "
+  time::
 }
-
-. ./time.sh
 
 date
 time::start
 time minikube start
-time::
 
 kubectl get pods -A
+await "kubectl get pods -A | grep Running | wc -l | grep 6"
 time kubectl create deployment nginx --image nginx
-time await "kubectl get pods -A | grep nginx | grep Running"
+await "kubectl get pods -A | grep nginx | grep Running"
 
 kubectl get pods -A
 
